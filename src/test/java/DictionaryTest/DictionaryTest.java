1091ed6532adeef95042727aa761644a53aee8d9
Emmanuela <kende.emmanuela@gmail.com>
2021-10-28 14:29:51 +0200

    fixture junit . 1

--- src/test/java/DictionaryTest/DictionaryTest.java
+++ src/test/java/DictionaryTest/DictionaryTest.java
@@ -1,8 +1,8 @@
 package DictionaryTest;
 import Dictionary.Dictionary;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,9 +12,14 @@ public class DictionaryTest {
     Dictionary dict;
 
     @Before
-    public  void setUpClass(){
+    public  void initialisation(){
         dict = new Dictionary("Example");
     }
+    @After
+    public void fin(){
+        dict = new Dictionary(null);
+    }
+
     @Test
     public void testDictionaryName() {
         assertThat(dict.getName(), equalTo("Example"));
@@ -31,6 +36,9 @@ public class DictionaryTest {
         assertThat(dict.getTranslation("contre"), equalTo("against"));
         assertThat(dict.getTranslation("salut"), equalTo("hello"));
     }
+
+
+
 
 
 }
